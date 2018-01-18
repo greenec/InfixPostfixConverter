70cf25c0a26022fdb35c7f52a00bd10946da73ef
Connor Greene <connor.greene2000@gmail.com>
2018-01-18 09:17:34 -0500

    Completed solution

--- InfixPostfixConverter/Program.cs
+++ InfixPostfixConverter/Program.cs
@@ -10,6 +10,59 @@ namespace InfixPostfixConverter
     {
         static void Main(string[] args)
         {
+            String input = Console.ReadLine().Replace(" ", String.Empty);
+            Stack<char> stack = new Stack<char>();
+            String operators = "+-*/()";
+
+            foreach (char t in input)
+            {
+                // t is an operand
+                if (operators.IndexOf(t) == -1)
+                {
+                    Console.Write(t);
+                }
+                else if (t == ')')
+                {
+                    while (true)
+                    {
+                        if (stack.Peek() == '(')
+                        {
+                            stack.Pop();
+                            break;
+                        }
+                        else
+                        {
+                            Console.Write(stack.Pop());
+                        }
+                    }
+                }
+                else
+                {
+                    while(true)
+                    {
+                        if (stack.Count == 0 || stack.Peek() == '(')
+                        {
+                            break;
+                        }
+
+                        // operator of lower precedence
+                        if(operators.IndexOf(stack.Peek()) < operators.IndexOf(t))
+                        {
+                            break;
+                        }
+
+                        Console.Write(stack.Pop());
+                    }
+                    stack.Push(t);
+                }
+            }
+
+            while(stack.Count > 0)
+            {
+                Console.Write(stack.Pop());
+            }
+
+            Console.ReadLine();
         }
     }
 }
